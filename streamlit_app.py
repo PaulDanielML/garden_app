449957f040af661fe0637dc16b90b539e6d99398
Paul Daniel <paudan22@gmail.com>
2022-05-06 13:14:57 +0200

    updates & refactorings

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
+from typing import Dict
 import pandas as pd
+import numpy as np
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
@@ -11,30 +13,54 @@ from utils import (
     make_colored_square,
     get_json_files,
     get_formatted_name,
+    show_map,
+    back_callback,
+    add_veggie_callback,
+    save_layout_as_image,
 )
 
 
-def main():
-    layout()
+def show_legend(base_layout: Dict, obj=None):
+    local_obj = obj or st
+    make_centered_title("Current plants", 20, local_obj)
+    local_obj.info("Here you can see the plant legend.")
 
+    COLS = ["name", "date", "color"]
+    df = pd.json_normalize(base_layout["objects"])
+    for col in COLS:
+        df[col] = df[col].astype("str")
+    col_1, col_2, col_3 = local_obj.columns([2, 6, 4])
 
-def layout():
+    col_1.subheader("Color")
+    col_2.subheader("Name")
+    col_3.subheader("Planted")
+
+    for name, group in df[COLS].groupby(["date", "name"]):
+        if name[1] == "Base Layout":
+            continue
+
+        # recreate the columns to get some spacing
+        col_1, col_2, col_3 = local_obj.columns([2, 6, 4])
+        color = group.color.iloc[0]
+        make_colored_square(color, obj=col_1)
+        col_2.write(name[1])
+        col_3.write(datetime.datetime.strptime(name[0], "%Y%m%d").date())
+
     st.sidebar.markdown("***")
-    make_centered_title("Current plants", 20, st.sidebar)
-    st.sidebar.info("Here you can see the plant legend.")
 
-    legend = st.sidebar.container()
 
-    st.sidebar.markdown("***")
-
+def add_new_veggie(base_layout: Dict):
+    st.session_state.show_add_button = False
     make_centered_title("Add New Veggie", 20, st.sidebar)
-
     st.sidebar.info(
-        "To add a new plant, enter a name, select which date is was planted and pick a color. Then draw it on the right and when you're done, click on 'Save Veggie'."
+        "To add a new plant, enter a name, select which date is was planted and pick a color. Then draw it on the \
+            right and when you're done, click on 'Save Veggie'."
     )
-    veggie_name = st.sidebar.text_input("Name")
-    planted_date = st.sidebar.date_input("Date planted", value=datetime.date.today())
-    fill_color = st.sidebar.color_picker("Fill color: ", value="#0E28D0")
+    form = st.sidebar.form("new_veggie_form")
+    veggie_name = form.text_input("Name")
+    planted_date = form.date_input("Date planted", value=datetime.date.today())
+    fill_color = form.color_picker("Fill color: ", value="#0E28D0")
+    add_button = form.form_submit_button("Save Veggie")
 
     # drawing_mode_ph = st.sidebar.empty()
     drawing_mode = st.sidebar.selectbox(
@@ -43,7 +69,79 @@ def layout():
     stroke_width = st.sidebar.slider("Border width: ", 1, 15, 4)
     # stroke_color = st.sidebar.color_picker("Border color: ")
 
-    add_button = st.sidebar.button("Save Veggie")
+    # col_1, col_2 = st.sidebar.columns(2)
+    # add_button = col_1.button("Save Veggie")
+    st.sidebar.button("Back", on_click=back_callback)
+    background_image = Image.open("img/background.png")
+    canvas_result = st_canvas(
+        fill_color=hex_to_rgb_str(fill_color),
+        stroke_width=stroke_width,
+        # stroke_color=stroke_color,
+        # background_color=bg_color,
+        background_image=background_image,
+        # background_image=Image.open("img/canvas_background.jpg"),
+        # update_streamlit=False,
+        height=1000,
+        # height=800
+        width=1600,
+        drawing_mode=drawing_mode,
+        initial_drawing=base_layout,
+        # initial_drawing=base_layout["geometry_data"],
+        # point_display_radius=point_display_radius if drawing_mode == "point" else 0,
+        key="canvas",
+    )
+    loaded_length = len(base_layout["objects"])
+
+    if add_button:
+        if veggie_name == "":
+            st.sidebar.error("Please enter a name.")
+        else:
+            new_objects = canvas_result.json_data["objects"][loaded_length:]
+
+            if len(new_objects) > 0:
+                for obj in new_objects:
+                    obj["name"] = veggie_name
+                    obj["date"] = planted_date.strftime("%Y%m%d")
+                    obj["color"] = fill_color
+
+            file_name = f'data/{datetime.datetime.now().strftime("%Y-%m-%d - %H:%M:%S")}.json'
+            objects_to_save = new_objects + base_layout["objects"]
+            to_save = canvas_result.json_data
+            to_save["objects"] = objects_to_save
+            with open(file_name, "w") as f:
+                json.dump(to_save, f, indent=4)
+
+            if canvas_result.image_data is not None:
+                save_layout_as_image(canvas_result.image_data)
+            back_callback()
+            st.experimental_rerun()
+
+
+def layout():
+    # st.write(st.session_state)
+    # st.sidebar.markdown("***")
+    if "layout_mode" not in st.session_state:
+        st.session_state.layout_mode = "legend"
+    if "show_add_button" not in st.session_state:
+        st.session_state.show_add_button = True
+    to_load = f"data/{get_json_files()[-1]}"
+    with open(to_load, "r") as f:
+        base_layout = json.load(f)
+
+    legend = st.sidebar.container()
+    # add_veggie = False
+    if st.session_state.show_add_button:
+        st.sidebar.button("Add New Veggie", on_click=add_veggie_callback)
+        # add_veggie = st.sidebar.button("Add New Veggie", on_click=add_veggie_callback)
+
+    if st.session_state.layout_mode == "add_veggie":
+        # if add_veggie or st.session_state.layout_mode == "add_veggie":
+        add_new_veggie(base_layout)
+    else:
+        make_centered_title("Garden Layout", 30)
+        st.image("img/current_layout.png")
+        show_legend(base_layout, legend)
+
     st.sidebar.markdown("***")
     make_centered_title("Create new / modify layout", 20, st.sidebar)
     st.sidebar.info("Here you can change the base layout (paths etc.).")
@@ -57,128 +155,34 @@ def layout():
     if modify_base:
         st.sidebar.warning("Not available yet.")
 
-    #     to_load = "data/0_base_layout.json"
-    #     drawing_mode = "transform"
-    #     save_modify = st.sidebar.button("Save")
-    # else:
-    to_load = f"data/{get_json_files()[-1]}"
-
-    # realtime_update = st.sidebar.checkbox("Update in realtime", True)
     st.sidebar.markdown("***")
 
-    make_centered_title("Garden Layout")
 
-    st.write(to_load)
-    # with open("data/0_base_layout.json", "r") as f:
-    with open(to_load, "r") as f:
-        base_layout = json.load(f)
-
-    # st.write(base_layout)
-
-    # st.write(base_layout)
-    loaded_length = len(base_layout["objects"])
-    st.write(loaded_length)
-    # st.write(type(base_layout))
-
-    canvas_result = st_canvas(
-        fill_color=hex_to_rgb_str(fill_color),
-        stroke_width=stroke_width,
-        # stroke_color=stroke_color,
-        # background_color=bg_color,
-        background_image=Image.open("img/background.png"),
-        # background_image=Image.open("img/canvas_background.jpg"),
-        # update_streamlit=realtime_update,
-        height=1000,
-        # height=800
-        width=1600,
-        drawing_mode=drawing_mode,
-        initial_drawing=base_layout,
-        # initial_drawing=base_layout["geometry_data"],
-        # point_display_radius=point_display_radius if drawing_mode == "point" else 0,
-        key="canvas",
+def map_page():
+    make_centered_title("Map", 30)
+    st.info(
+        "The garden can be a bit hard to find the first time you go there, as it's not were you will land if you search for 'Grantoftegaard' \
+        on Google. Instead, you can navigate to 'Grantoftegård (Stalde)'."
     )
-
-    if add_button:
-        if veggie_name == "":
-            st.sidebar.error("Please enter a name.")
-        else:
-            # canvas_result.json_data["objects"][0]["test"] = "asdölkfgöslkdfklöjasölkdjfölkjasdfjkl"
-            new_objects = canvas_result.json_data["objects"][loaded_length:]
-
-            if len(new_objects) > 0:
-                for obj in new_objects:
-                    obj["name"] = veggie_name
-                    obj["date"] = planted_date.strftime("%Y%m%d")
-                    obj["color"] = fill_color
-
-            st.write(new_objects)
-
-            file_name = f'data/{datetime.datetime.now().strftime("%Y-%m-%d - %H:%M:%S")}.json'
-            objects_to_save = new_objects + base_layout["objects"]
-            to_save = canvas_result.json_data
-            to_save["objects"] = objects_to_save
-            with open(file_name, "w") as f:
-                json.dump(to_save, f, indent=4)
-
-    # for obj in canvas_result.json_data["objects"]:
-    #     obj["name"] = veggie_name
-    #     obj["date"] = planted_date.strftime("%Y%m%d")
-
-    # st.write(canvas_result.json_data)
-
-    # else:
-    #     test = PlantField(
-    #         geometry_data=canvas_result.json_data,
-    #         color=fill_color,
-    #         name=veggie_name,
-    #         date=planted_date.strftime("%Y%m%d"),
-    #     )
-    #     st.write(test.json())
-
-    # file_name = f"data/0_base_layout.json"
-
-    # Do something interesting with the image data and paths
-    # if canvas_result.image_data is not None:
-    # st.image(canvas_result.image_data)
-
-    # if canvas_result.json_data is not None:
-    COLS = ["name", "date", "color"]
-    df = pd.json_normalize(base_layout["objects"])
-    for col in COLS:
-        df[col] = df[col].astype("str")
-    # st.dataframe(objects[COLS])
-
-    # for row in df[COLS].itertuples():
-    # st.write(row)
-    col_1, col_2, col_3 = legend.columns([3, 5, 5])
-
-    col_1.subheader("Color")
-    col_2.subheader("Name")
-    col_3.subheader("Planted")
-
-    for name, group in df[COLS].sort_values("date").groupby(["name", "date"]):
-        if name[0] == "Base Layout":
-            continue
-        color = group.color[0]
-        name_str = get_formatted_name(name[0])
-
-        make_colored_square(color, obj=col_1)
-        col_2.write(name_str)
-        col_3.write(datetime.datetime.strptime(name[1], "%Y%m%d").date())
-        # make_colored_square("#000000", obj=col_1)
-        # make_colored_square("#000000", obj=col_2)
-
-    # col_2.write(get_formatted_name("aösldfjasödf ölasdjfölasdlfj"))
-    # col_2.write("aösldfjasödfölasdjfölasdlfj")
-    # col_3.write(datetime.datetime.strptime(name[1], "%Y%m%d").date())
+    show_map((55.737438, 12.335938))
 
 
-if __name__ == "__main__":
+PAGES = {"Garden Layout": layout, "Map": map_page}
+
+
+def main():
     st.set_page_config(
         page_title="Grantoftegaard",
         page_icon="https://st4.depositphotos.com/2046901/20637/v/1600/depositphotos_206373408-stock-illustration-spring-plant-icon-vector-illustration.jpg",
         layout="wide",
     )
     make_centered_title("Grantoftegaard Garden", 40)
-    # st.sidebar.subheader("Configuration")
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    st.sidebar.markdown("***")
+
+    PAGES[selection]()
+
+
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,8 +1,46 @@
 import os
-from typing import Dict, List
-import datetime
-import re
+from typing import Dict, Tuple
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
+import numpy as np
+from PIL import Image
+
+
+def save_layout_as_image(image_data: np.ndarray, file_name: str = "current_layout.png"):
+    img = Image.fromarray(image_data, mode="RGBA")
+    background_image = Image.open("img/background.png")
+    out = background_image.resize(size=(img.width, img.height))
+    mask = img.getchannel(3)
+    mask = mask.point(lambda p: 230 if p > 0 else 0)
+    img = img.convert("RGB")
+    out.paste(img, (0, 0), mask)
+    out.save(f"img/{file_name}")
+
+
+def add_veggie_callback():
+    st.session_state.layout_mode = "add_veggie"
+    st.session_state.show_add_button = False
+
+
+def back_callback():
+    st.session_state.layout_mode = "legend"
+    st.session_state.show_add_button = True
+
+
+def show_map(coordinates: Tuple):
+    my_map = folium.Map(location=coordinates, zoom_start=15)
+
+    google_satellite_hybrid = folium.TileLayer(
+        tiles="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+        attr="Google",
+        name="Google Satellite",
+        overlay=True,
+        control=True,
+    )
+    google_satellite_hybrid.add_to(my_map)
+    folium.Marker(coordinates, popup="Our Garden").add_to(my_map)
+    folium_static(my_map, width=1200, height=800)
 
 
 def get_formatted_name(name: str, line_length: int = 15):
@@ -12,7 +50,7 @@ def get_formatted_name(name: str, line_length: int = 15):
 
 
 def get_json_files():
-    return sorted(os.listdir("data"))
+    return sorted([f for f in os.listdir("data") if f.endswith(".json")])
 
 
 def make_centered_title(text: str, size: int = 25, obj=None):
@@ -39,6 +77,17 @@ def make_colored_square(hex_color: str, size: int = 30, obj=None):
         f'<div style="height:{size}px;width:{size}px;background-color:{hex_color};border-radius:10px 10px 10px 10px;border-style:dotted;border-color:#19191A"></div>',
         unsafe_allow_html=True,
     )
+
+
+def v_spacer(height, obj=None, sb=False) -> None:
+    for _ in range(height):
+        if obj is not None:
+            obj.write("\n")
+        else:
+            if sb:
+                st.sidebar.write("\n")
+            else:
+                st.write("\n")
 
 
 if __name__ == "__main__":
